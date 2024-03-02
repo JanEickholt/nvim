e99728d0967afdc1e4f2abc1ca7c2fc5c78ce25b
Hexdigest123 <pierre.maurice.m256@googlemail.com>
2024-03-02 15:09:54 +0100

    changed lazy in vim-dadbod/-ui

--- lua/plugins/vim-dadbod-ui.lua
+++ lua/plugins/vim-dadbod-ui.lua
@@ -1,8 +1,8 @@
 return {
   "kristijanhusak/vim-dadbod-ui",
   dependencies = {
-    { "tpope/vim-dadbod", lazy = true },
-    { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+    { "tpope/vim-dadbod", lazy = false },
+    { "kristijanhusak/vim-dadbod-completion", lazy = false },
   },
   cmd = {
     "DBUI",
