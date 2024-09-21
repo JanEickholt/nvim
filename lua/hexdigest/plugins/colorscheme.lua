2e81bfdfcf8ba08f14659abdd6999d442783341e
Jan Eickholt <eickholtjan@gmail.com>
2024-09-22 00:46:05 +0200

    Changed colorscheme to xrdb background

--- lua/hexdigest/plugins/colorscheme.lua
+++ lua/hexdigest/plugins/colorscheme.lua
@@ -1,8 +1,18 @@
 return {
   "catppuccin/nvim",
   lazy = false,
+  opts = {
+    transparent_background = false,
+    color_overrides = {
+      mocha = {},
+    },
+  },
   priority = 1000,
-  config = function()
+  config = function(_, opts)
+    local handle = io.popen("xrdb -query | grep -w '*background' | awk '{print $2}'")
+    opts.color_overrides.mocha.base = handle:read("*a"):gsub("\n", "")
+    handle:close()
+    require("catppuccin").setup(opts)
     vim.cmd("colorscheme catppuccin-mocha")
   end,
 }
