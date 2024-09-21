626b065201c4a28efe935409a06ef2f3e9b9b591
Jan Eickholt <eickholtjan@gmail.com>
2024-09-22 00:46:48 +0200

    Added .rasi to colorizer to conveniently edit rofi files

--- lua/hexdigest/plugins/colorizer.lua
+++ lua/hexdigest/plugins/colorizer.lua
@@ -1,6 +1,17 @@
+vim.filetype.add({
+  extension = {
+    rasi = "rasi",
+  },
+})
+
 return {
   "norcalli/nvim-colorizer.lua",
   config = function()
-    require("colorizer").setup({ "*" })
+    require("colorizer").setup({
+      "*",
+    }, {
+      names = false, -- Disable name-based colors globally
+      RRGGBBAA = true, -- Enable RRGGBBAA colors globally
+    })
   end,
 }
