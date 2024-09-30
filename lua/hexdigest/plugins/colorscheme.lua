4af34cf9a96906b3139ae2c26c69d707b6455ff2
Jan Eickholt <eickholtjan@gmail.com>
2024-09-30 06:54:03 +0200

    Added a fallback color to colorscheme.lua

--- lua/hexdigest/plugins/colorscheme.lua
+++ lua/hexdigest/plugins/colorscheme.lua
@@ -10,8 +10,15 @@ return {
   priority = 1000,
   config = function(_, opts)
     local handle = io.popen("xrdb -query | grep -w '*background' | awk '{print $2}'")
-    opts.color_overrides.mocha.base = handle:read("*a"):gsub("\n", "")
+    local background_color = handle:read("*a"):gsub("\n", "")
     handle:close()
+
+    if background_color == "" then
+      background_color = "#1e1e2e"
+    end
+
+    opts.color_overrides.mocha.base = background_color
+
     require("catppuccin").setup(opts)
     vim.cmd("colorscheme catppuccin-mocha")
   end,
