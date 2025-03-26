a0f27259805b6c64f2181b49d856a27eba502614
Jan Eickholt <eickholtjan@gmail.com>
2025-03-26 17:47:53 +0100

    Merge 2

--- lua/hexdigest/plugins/formatter.lua
+++ lua/hexdigest/plugins/formatter.lua
@@ -1,8 +1,8 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local conform = require("conform")
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
 
     conform.setup({
       formatters_by_ft = {
@@ -31,12 +31,14 @@ return {
       },
     })
 
-    vim.keymap.set({ "n", "v" }, "<leader>f", function()
-      conform.format({
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      })
-    end, { desc = "Format file or range (in visual mode)" })
-  end,
+>>>>>>> ade5a8fe4fb101ced45bca1c27df3f07d9fd151b
+
+		vim.keymap.set({ "n", "v" }, "<leader>f", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
 }
