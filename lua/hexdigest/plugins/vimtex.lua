5ac5da40d3b6e49c0217ffbea4f539f49701625a
Jan Eickholt <eickholtjan@gmail.com>
2025-03-27 22:08:14 +0100

    Added back vimtex mappings, cuz they useful

--- lua/hexdigest/plugins/vimtex.lua
+++ lua/hexdigest/plugins/vimtex.lua
@@ -3,7 +3,7 @@ return {
   init = function()
     vim.g["vimtex_view_method"] = "zathura"
     vim.g["vimtex_quickfix_mode"] = 0
-    vim.g["vimtex_mappings_enabled"] = 0
+    vim.g["vimtex_mappings_enabled"] = 1
     vim.g["vimtex_indent_enabled"] = 0
     vim.g["tex_flavor"] = "latex"
     vim.g["tex_indent_items"] = 0
