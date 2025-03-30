c2781322eca67ec347ee8984a5d5d3940ac7f6ed
Hexdigest123 <pierre@merckel.dev>
2025-03-31 00:34:55 +0200

    CHG: Removed autopairs and added linter configs

--- lua/hexdigest/plugins/nvim-lint.lua
+++ lua/hexdigest/plugins/nvim-lint.lua
@@ -1,16 +1,18 @@
 return {
-	"mfussenegger/nvim-lint",
-	config = function()
-		require("lint").linters_by_ft = {
-			python = { "pylint" },
-			php = { "phpcs" },
-			go = { "trivy" },
-		}
+  "mfussenegger/nvim-lint",
+  config = function()
+    require("lint").linters_by_ft = {
+      python = { "pylint" },
+      php = { "phpcs" },
+      go = { "trivy" },
+      c = { "trivy" },
+      cpp = { "trivy" },
+    }
 
-		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-			callback = function()
-				require("lint").try_lint()
-			end,
-		})
-	end,
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+      callback = function()
+        require("lint").try_lint()
+      end,
+    })
+  end,
 }
