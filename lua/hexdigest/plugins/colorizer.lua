64734e6a05280388530210a64b99f74542371b7b
Jan Eickholt <eickholtjan@gmail.com>
2025-05-06 14:18:12 +0200

    CHG: add rgba, disable named colorization - support for easier file type addition, if they don't work out of the   box

--- lua/hexdigest/plugins/colorizer.lua
+++ lua/hexdigest/plugins/colorizer.lua
@@ -1,6 +1,17 @@
+vim.filetype.add({
+  extension = {
+    rasi = "rasi",
+  },
+})
+
 return {
   "norcalli/nvim-colorizer.lua",
   config = function()
-    require("colorizer").setup({ "*" })
+    require("colorizer").setup({
+      "*",
+    }, {
+      names = false,
+      RRGGBBAA = true,
+    })
   end,
 }
