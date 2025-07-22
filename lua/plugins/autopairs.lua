ca2ff78e1896c90dad0806dd1dc1de342522f2a6
Jan Eickholt <eickholtjan@gmail.com>
2025-07-22 23:24:01 +0200

    feat: allow whitespace in autopairs jump

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -10,19 +10,43 @@ return {
 
     vim.keymap.set("i", "jk", function()
       local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-      local line = vim.api.nvim_get_current_line()
+      local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+      local total_lines = #lines
 
-      local next_char = line:sub(col + 1, col + 1)
       local closing_chars = { '"', "'", ")", "}", "]", "`" }
 
-      for _, char in ipairs(closing_chars) do
-        if next_char == char then
-          vim.api.nvim_win_set_cursor(0, { row, col + 1 })
-          return
+      local function is_whitespace(char)
+        return char:match("%s") ~= nil
+      end
+
+      local current_row, current_col = row, col + 1
+
+      while current_row <= total_lines do
+        local line = lines[current_row] or ""
+
+        for i = current_col, #line do
+          local char = line:sub(i, i)
+
+          if not is_whitespace(char) then
+            for _, closing_char in ipairs(closing_chars) do
+              if char == closing_char then
+                vim.api.nvim_win_set_cursor(0, { current_row, i })
+                return
+              end
+            end
+            break
+          end
+        end
+
+        current_row = current_row + 1
+        current_col = 1
+
+        if current_row > row + 10 then
+          break
         end
       end
 
       vim.api.nvim_feedkeys("jk", "n", false)
-    end, { desc = "Jump out of autopairs or insert jk" })
+    end, { desc = "Jump out of autopairs (skips whitespace) or insert jk" })
   end,
 }
