b18d0d218497a4ed80249c39416edd77f64e2db2
Jan Eickholt <eickholtjan@gmail.com>
2026-05-09 01:27:57 +0200

    fix: remove rustfmt

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,56 +1,55 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    require("mason").setup()
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		require("mason").setup()
 
-    require("mason-lspconfig").setup({
-      automatic_installation = true,
-      ensure_installed = {},
-      automatic_enable = false,
-    })
+		require("mason-lspconfig").setup({
+			automatic_installation = true,
+			ensure_installed = {},
+			automatic_enable = false,
+		})
 
-    require("mason-tool-installer").setup({
-      ensure_installed = {
-        "typescript-language-server",
-        "gopls",
-        "dockerls",
-        "svelte-language-server",
-        "tailwindcss-language-server",
-        "jdtls",
-        "texlab",
-        "intelephense",
-        "clangd",
-        "pyright",
-        "lua-language-server",
+		require("mason-tool-installer").setup({
+			ensure_installed = {
+				"typescript-language-server",
+				"gopls",
+				"dockerls",
+				"svelte-language-server",
+				"tailwindcss-language-server",
+				"jdtls",
+				"texlab",
+				"intelephense",
+				"clangd",
+				"pyright",
+				"lua-language-server",
 
-        -- Formatters
-        "prettierd",
-        "stylua",
-        "black",
-        "clang-format",
-        "pretty-php",
-        "rustfmt",
-        "goimports",
+				-- Formatters
+				"prettierd",
+				"stylua",
+				"black",
+				"clang-format",
+				"pretty-php",
+				"goimports",
 
-        -- Linters
-        "pylint",
-        "phpcs",
+				-- Linters
+				"pylint",
+				"phpcs",
 
-        -- Debuggers
-        "codelldb",
-        "debugpy",
+				-- Debuggers
+				"codelldb",
+				"debugpy",
 
-        -- Java Development
-        "java-debug-adapter",
-        "java-test",
+				-- Java Development
+				"java-debug-adapter",
+				"java-test",
 
-        -- Java Formatting
-        "google-java-format",
-      },
-    })
-  end,
+				-- Java Formatting
+				"google-java-format",
+			},
+		})
+	end,
 }
